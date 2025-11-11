fc3d4476120bf9eaaff9967d082194cc901aaf67
Martijn van der Wijst <martijnvanderwijst@gmail.com>
2025-11-11 15:30:36 +0100

    chore(og-image): remove unused alt export; re-export size/contentType from renderer

--- app/en/opengraph-image.tsx
+++ app/en/opengraph-image.tsx
@@ -1,11 +1,10 @@
 import { i18n } from '@/lib/i18n'
-import { createImageResponse, size as ogSize, contentType as ogContentType } from '@/lib/opengraph-renderer'
+import { createImageResponse } from '@/lib/opengraph-renderer'
+import { size, contentType } from '@/lib/opengraph-renderer'
 
 const t = i18n.en
 
-export const alt = t.seo.title
-export const size = ogSize
-export const contentType = ogContentType
+export { size, contentType }
 
 export default async function Image() {
   return createImageResponse(t)
